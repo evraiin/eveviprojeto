c20a1d1975eef83fa363109aa7c157534ea5cab9
evraiin <outunoq@gmail.com>
2025-01-08 16:28:00 -0300

    mudei server

--- dao/__init__.py
+++ dao/__init__.py
@@ -3,86 +3,73 @@ import psycopg2
 def conectardb():
     con = psycopg2.connect(
         host='localhost',
-        database='consultaifpb',
+        database='clinicavet',
         user='postgres',
         password='12345'
     )
     return con
 
-conectardb()
-
 
 def inserirusuario(nome, email, senha):
- conexao = conectardb()
- cur = conexao.cursor()
- exito = False
+    conexao = conectardb()
+    cur = conexao.cursor()
+    exito = False
+    try:
+        sql = f"INSERT INTO usuario (nome, email, senha) VALUES ('{nome}', '{email}', '{senha}')"
+        cur.execute(sql)
+    except psycopg2.Error:
+        conexao.rollback()
+        exito = False
+    else:
+        conexao.commit()
+        exito = True
 
- try:
-  sql = f"INSERT INTO usuario (nome, email, senha) VALUES ('{nome}', '{email}','{senha}')"
-  cur.execute(sql)
- except psycopg2.Error:
-  conexao.rollback()
-  exito = False
- else:
-   conexao.commit()
-   exito = True
-   conexao.close()
- return exito
+    conexao.close()
+    return exito
 
 
 def verificarlogin(email, senha):
     conexao = conectardb()
     cur = conexao.cursor()
-    cur.execute(f”SELECT
-    email, nome
-    FROM
-    usuario
-    WHERE
-    email = ‘{email}’ AND
-    senha = ‘{senha}’”)
+    cur.execute(f"SELECT email, nome FROM usuario WHERE email = '{email}' AND senha = '{senha}'")
     recset = cur.fetchall()
     cur.close()
     conexao.close()
+
     return recset
 
 
-def inseriragendamento(nome, telefone, email, dia_consulta, nome_pet, especie,
-                       motivo):
-    conexao = conectardb()
-    cur = conexao.cursor()
+def inserir_agendamento(nome, telefone, email, dia_consulta, nome_pet, especie, motivo, conexao):
     exito = False
     try:
-        sql = f"INSERT INTO agendamento (nome, telefone, emai, nome_pet, especie,
-motivo) VALUES('{nome}', '{telefone}', '{email}', '{dia_consulta}', '{nome_pet}',
-'{motivo')"
-cur.execute(sql)
-except psycopg2.Error:
-conexao.rollback()
-exito = False
-else:
-conexao.commit()
-exito = True
-conexao.close()
-return exito
+        sql = """
+            INSERT INTO agendamento (nome, telefone, email, nome_pet, especie, motivo) 
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        cur = conexao.cursor()
+        cur.execute(sql, (nome, telefone, email, nome_pet, especie, motivo))
+    except psycopg2.Error as err:
+        print(f"Erro ao executar a consulta: {err}")
+        conexao.rollback()
+        exito = False
+    else:
+        conexao.commit()
+        exito = True
+    finally:
+        cur.close()
+        conexao.close()
+    return exito
 
 
-def get_usuario_por_nome(self, nome_usuario):
+def get_usuario_por_nome(connection, nome_usuario):
     try:
-        cursor = self.connection.cursor(dictionary=True)
-    query = “SELECT * FROM
-    usuarios
-    WHERE
-    nome_usuario = %s”
-    cursor.execute(query, (nome_usuario,))
-    resultado = cursor.fetchone()  # Obtém um único registro
-    cursor.close()
-    return resultado
+        cursor = connection.cursor(dictionary=True)
+        query = "SELECT * FROM usuario WHERE nome_usuario = %s"
+        cursor.execute(query, (nome_usuario,))
+        resultado = cursor.fetchone()  # Obtém um único registro
+        return resultado
     except mysql.connector.Error as err:
-    print(f”Erro
-    ao
-    acessar
-    o
-    banco
-    de
-    dados: {err}”)
-    return None
+        print(f"Erro ao acessar o banco de dados: {err}")
+        return None
+    finally:
+        cursor.close()
--- servidor.py
+++ servidor.py
@@ -1,7 +1,7 @@
 #importando o flask
+import psycopg2
 from flask import *
 import dao
-
 
 app = Flask(__name__)
 app.secret_key = 'X8f4Z1p9LdWvHo3T'
@@ -11,152 +11,79 @@ def pageprincipal():
     return render_template('index.html')
 
 
-@app.route('/')
-def pageprincipal():
-    return render_template('index.html')
-
-
-
 @app.route('/cadastro', methods=['POST'])
 def cadastro():
     nome = request.form.get('nome')
     email = request.form.get('email')
     senha = request.form.get('senha')
     if dao.inserirusuario(nome, email, senha):
-        msg = 'usuário cadastrado com sucesso'
+        msg = 'Usuário cadastrado com sucesso'
     else:
-        msg = 'problemas ao inserirusuário'
+        msg = 'Problemas ao cadastrar o usuário'
     return render_template('index.html', mensagem=msg)
 
 
-@app.route('/login', methods = ['post'])
-
-
-
+@app.route('/login', methods=['POST'])
 def login():
-
     email = request.form.get('email')
-
     senha = request.form.get('senha')
-
-
 
     resultado = dao.verificarlogin(email, senha)
 
-
-
-    if len(resultado) > 0:
-
+    if resultado and len(resultado) > 0:
         session['login'] = resultado[0][1]
-
-        return render_template('logon.html', user = resultado[0][1])
-
+        return render_template('logon.html', user=resultado[0][1])
     else:
-
-        msg = 'senha ou login incorretos'
-
-        return render_template('index.html', msglogin = msg)
+        msg = 'Senha ou login incorretos'
+        return render_template('index.html', msglogin=msg)
 
 
-
-@app.route('/agendamento', methods = ['post'])
-
-
-
+@app.route('/agendamento', methods=['POST'])
 def agendamento():
+    nome = request.form.get('nome')
+    telefone = request.form.get('telefone')  # Corrigido uso de aspas simples
+    email = request.form.get('email')
+    dia_consulta = request.form.get('dia-consulta')
+    nome_pet = request.form.get('nome-pet')
+    especie = request.form.get('especie')
+    motivo = request.form.get('motivo')
 
-  nome = request.form.get('nome')
-
-  telefone = request.form.get(‘telefone’)
-
-  email = request.form.get('email')
-
-  dia_consulta = request.form.get('dia-consulta')
-
-  nome_pet = request.form.get('nome-pet')
-
-  especie = request.form.get('especie')
-
-  motivo = request.form.get('motivo')
-
-
-
-
-
-    if dao.inseriragendamento(nome, telefone, e-mail, nome_pet, especie, motivo):
-
-       msg = 'agendamento realizado com sucesso'
-
+    if dao.inseriragendamento(nome, telefone, email, dia_consulta, nome_pet, especie, motivo):
+        msg = 'Agendamento realizado com sucesso'
     else:
-
-       msg = 'problemas ao agendar'
-
-    return render_template('logon.html', mensagem = msg)
-
-
-
+        msg = 'Problemas ao realizar agendamento'
+    return render_template('logon.html', mensagem=msg)
 
 
 @app.route('/visualizar_informacoes')
-
 def visualizar_informacoes():
-
-  
-
-    dao = DAO()
-
-    informacoes = dao.get_informacoes() 
-
+    informacoes = dao.get_informacoes()
     return render_template('informacoes.html', informacoes=informacoes)
 
 
-
-
-
 def insert_comentario(login, comentario, conexao):
-
-
-
     cur = conexao.cursor()
-
     exito = False
-
     try:
-
-    
-
-        sql = (f"UPDATE usuario SET comentario = '{comentario}' where login = '{login}'")
-
+        sql = f"UPDATE usuario SET comentario = '{comentario}' WHERE login = '{login}'"
         cur.execute(sql)
-
     except psycopg2.IntegrityError:
-
         conexao.rollback()
-
         exito = False
-
     else:
-
         conexao.commit()
-
         exito = True
-
-
-
-    cur.close()
-
-    conexao.close()
-
+    finally:
+        cur.close()
+        conexao.close()
     return exito
-
 
 
 @app.route('/logout')
 def logout():
-    session.clear()  
-    return redirect('/')  
+    session.clear()
+    return redirect('/')
 
 
-app.run(debug=True)
- 
-
+if __name__ == '_main_':
+    app.run(debug=True)
